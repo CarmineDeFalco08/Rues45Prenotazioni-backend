f4d21191b71a732776e195d22de2346f5fa09251
Carmine De Falco <defalco.carmine2008@gmail.com>
2026-06-14 18:28:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -224,7 +224,13 @@ app.post('/api/prenota', async (req, res) => {
         // 4. Avvia Puppeteer per creare il PDF
         const browser = await puppeteer.launch({ 
             headless: "new",
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
+            executablePath: '/usr/bin/chromium-browser', // <--- Usa il browser di sistema Linux
+            args: [
+                '--no-sandbox', 
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu'
+            ]
         });
         const page = await browser.newPage();
         
